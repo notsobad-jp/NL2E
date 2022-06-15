e399140745906a0fcdd154bc4591d617b86840aa
notsobad <tomomichi.onishi@gmail.com>
2022-06-15 23:27:18 +0900

    remove old files

--- Docker/teste/xeyes.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm -it \
-    --net host \
-    -e DISPLAY=$DISPLAY \
-    -v $HOME/.Xauthority:/root/.Xauthority \
-    xeyes-ubuntu
--- jupyter.sh
+++
@@ -1,2 +0,0 @@
-echo docker run --rm -p 8888:8888 -v `pwd`/KHCoder/works/jupyter:/home/jovyan/work jupyter/minimal-notebook
-docker run --rm -p 8888:8888 -v `pwd`/KHCoder/works/jupyter:/home/jovyan/work jupyter/minimal-notebook
